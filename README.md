e70aa627a4d1052f722484ecd47c37bdc9c3e941
Kanha Tomar <kanharajput91@gmail.com>
2023-08-29 16:38:16 +0530

    useEffect hook

---
+++ README.md
@@ -0,0 +1,6 @@
+### useEffect
+
+useEffect is used to run when certain conditions matches. Normally it has two parameters, one define the code to be  
+executed when certain event happens and another one is called as dependencies it defines what are the events which  
+after which we have to re-run the useEffect hook. If nothing pass in the dependencies then it will only run at a time  
+of initialise render.
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,14 +7,25 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // check that the user is already logged in
+  // it will also run when we refresh the page.
+  useEffect(() =>{
+    if(localStorage.getItem('isLoggedIn') === '1'){
+      setIsLoggedIn(true);
+    }
+  },[]);
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    // save to local storage of after refreshing of the page 
+    // we can check this data to known user was loggedIn or not
+    localStorage.setItem('isLoggedIn', '1');       
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    // remove the saved loggedIn status
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (
